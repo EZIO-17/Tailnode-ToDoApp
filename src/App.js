905cb7e15d27a91149acf63a49a3312fcf0cbbcc
unknown <sanyam@xtenav.com>
2022-09-16 16:19:30 +0530

    Create a note on on pressing ENTER key

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 // import logo from './logo.svg';
+import { faPassport } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
 
-  var input = document.querySelector("input");
-  var btn = document.querySelector(".addTask > button");
+  function enterTask(event){
+      if (event.key === 'Enter') {
+        addList(event);
+      }
+  }
 
-  btn.addEventListener("click", addList);
-
-  // // input.addEventListener("keyup", (e) => {
-  // //     e.keyCode === 13 ? addList(e) : null;
-  // // });
-
-  function addList(e) {
+  function addList(_e) {
+      var input = document.querySelector("input");
       var notcomp = document.querySelector(".notcomp");
       var comp = document.querySelector(".comp");
 
       var newli = document.createElement("li");
       var checkBtn = document.createElement("button");
       var delBtn = document.createElement("button");
-
-      checkBtn.innerHTML = `<i class="fas fa-check-circle"></i>`;
-      delBtn.innerHTML = `<i class="fas fa-trash"></i>`;
-
+      console.log("4" , checkBtn)
+      checkBtn.innerHTML = `Done`;
+      delBtn.innerHTML = `<button>Remove</button>`;
+      console.log("1222",input)
       if (input.value !== "") {
           newli.textContent = input.value;
           input.value = "";
@@ -46,8 +45,8 @@ function App() {
   return (
     <div className="cont">
       <div className="addTask">
-          <input type="text" placeholder="Add a Task" />
-          <button>Add</button>
+          <input id='taskInput' type="text" placeholder="Add a Task" onKeyDown={enterTask}/>
+          <button onClick={addList}>Add</button>
       </div>
       <ol className="notcomp">
           <h3>Task Not Completed</h3>
